5bc8388964e6faea5ee87d2d9175a5079334aeed
fanzhongwei <fanzhongwei@thunisoft.com>
2023-10-09 18:19:44 +0800

    书籍下载

---
+++ com-teddy-python/scrapy_spider/book_collector/main.py
@@ -0,0 +1,17 @@
+from scrapy.cmdline import execute
+import os
+import sys
+
+if __name__ == '__main__':
+    # 获取当前脚本路径
+    dirpath = os.path.dirname(os.path.abspath(__file__))
+    #运行文件绝对路径
+    print(os.path.abspath(__file__))
+    #运行文件父路径
+    print(dirpath)
+    # 添加环境变量
+    sys.path.append(dirpath)
+    #切换工作目录
+    os.chdir(dirpath)
+
+    execute(['scrapy','crawl','zhetian'])
