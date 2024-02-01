f3fcfdacaf4b536ff1836ac781af514f07d332ba
fanzhongwei <fanzhongwei@thunisoft.com>
2024-02-01 18:51:31 +0800

    下载数据重试

--- com-teddy-python/scrapy_spider/book_collector/book_collector/spiders/wanmeishijie.py
+++ com-teddy-python/scrapy_spider/book_collector/book_collector/spiders/wanmeishijie.py
@@ -5,15 +5,23 @@ import random
 
 class ZhetianSpider(scrapy.Spider):
     name = "wanmeishijie"
-    allowed_domains = ["ibiquges.com"]
-    start_urls = ["https://www.ibiquges.com/8/8345/3734159.html"]
-    end_next_page = '/8/8345/'
-    base_url = 'http://www.ibiquges.com'
+    allowed_domains = ["yadaiercn.com"]
+    start_urls = ["http://www.yadaiercn.com/ldks/16478/4501007.html"]
+    base_page = '/ldks/16478/'
+    end_page = '4532422.html'
+    base_url = 'http://www.yadaiercn.com'
     book_name = '完美世界.txt'
 
     def parse(self, response):
+        if response.status == 503:
+            print(f'save {response._url} 失败，即将开始重试')
+            time.sleep(random.uniform(1,3))
+            r = response.request.copy()
+            r.dont_filter = True
+            yield r
+            return
         
-        if self.saveParagraph(response=response) is False:
+        if (not response.text or self.saveParagraph(response=response) is False):
             print(f'save {response._url} 失败，即将开始重试')
             time.sleep(random.uniform(1,3))
             r = response.request.copy()
@@ -22,35 +30,48 @@ class ZhetianSpider(scrapy.Spider):
             return
 
 
-        next_page = response.xpath('//div[@class="bottem1"]//a[text()="下一章"]/@href').extract_first()
-        if len(next_page) <= len(self.end_next_page):
+        next_page = response.xpath('//div[@class="section-opt m-bottom-opt"]//a[text()="下一章"]/@href').extract_first()
+        if next_page is None:
+            next_page = response.xpath('//div[@class="section-opt m-bottom-opt"]//a[text()="下一页"]/@href').extract_first()
+        
+        if next_page == self.end_page:
             print(f'find completed {next_page}')
         else:
+            if next_page.startswith(self.base_page) is False:
+                next_page = self.base_page + next_page
             print(f'find next chapter {self.base_url}{next_page}')
-            time.sleep(random.uniform(1,3))
+            #time.sleep(random.uniform(1,3))
             yield scrapy.Request(self.base_url + next_page, callback=self.parse)
 
     def saveParagraph(self, response):
-        title = response.css(".bookname>h1::text").get()
+        title = response.css(".title::text").get()
         if title is None:
             return False
         
+        is_next_page = response.xpath('//div[@class="section-opt m-bottom-opt"]//a[text()="下一页"]/@href').extract_first() is None
+
         title = title.strip()
-        print(f'find chapter {title}')
+        if is_next_page is True:
+            print(f'find chapter {title}')
+        else:
+            print(f'find chapter {title} 还有下一页')
+        
         with open(self.book_name, 'a', encoding='utf-8') as f:
-            f.write(title)
-            f.write('\r\n\r\n')
+            if is_next_page is False:
+                f.write(title)
+                f.write('\r\n\r\n')
 
             contents = response.css("#content::text")
 
             for content in contents:
-                paragraph = content.get().replace('\xa0','').replace('\r','')
+                paragraph = content.get().replace('\xa0','').replace('\r','').strip()
                 if paragraph == '':
                     continue
                 # print(paragraph)
                 f.write('\t' + paragraph)
                 f.write('\r\n')
             
-            f.write('\r\n\r\n')
+            if is_next_page is True:
+                f.write('\r\n\r\n')
         return True
 
